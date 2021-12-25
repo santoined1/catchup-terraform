778b1981f419b0fca67fe69f2d7d30db0b6b08f2
santoined1 <96506147+santoined1@users.noreply.github.com>
2021-12-25 15:16:00 -0500

    an ec2 instance added to code

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+resource "aws_instance" "pub" {
+  ami = "0ed9277fb7eb570c9"
+  instance_type =" t2.micro"
+  
+}
+
 resource "aws_s3_bucket" "data_team_bucket" {
   bucket = var.bucket_name
   acl    = "private"
